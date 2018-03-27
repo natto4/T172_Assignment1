6695646e3ea554be4628cf548d1bcc6b9977d576
Gradleee <37685721+Gradleee@users.noreply.github.com>
2018-03-27 19:41:23 +1100

    Update codeprocessor.js

--- js/codeprocessor.js
+++ js/codeprocessor.js
@@ -5,44 +5,37 @@
  *   	event.detail.data and the timestamp in event.timeStamp
  */
 
-var listenData = {
+var listenData = { //data that will be used by the translator
     blackWhite: [0],
-    timeTaken: ["start"],
-    halfGap: 0,
-    fullGap: 0,
+    timeTaken: [0],
 };
 
 _listen = function(event)
 {
     
-    var greyAverage = (event.detail.data[0] + event.detail.data[1] + event.detail.data[2])/3
-    let time1 = 0, time2 = 0, tempTime = 0;
+	// Declaring variables
+    var greyAverage = (event.detail.data[0] + event.detail.data[1] + event.detail.data[2])/3 //converts pixel data to black and white
+    var time1 = 0, time2 = 0;
+
     if(greyAverage > 127.5 && listenData.blackWhite[listenData.blackwhite.length-1]===0) //was black and is now white
         {
             listenData.blackWhite.push(1); //record that image is now white
-            if(time2 != 0)
-            {
-                time1 = event.timeStamp; //record time
-            }
             
-            listenData.timeTaken.push("tap");
+            time1 = event.timeStamp; //record time
+            
+            if(time2 != 0) //accounting for the first case where time2 = 0
+            {
+                listenData.timeTaken.push(Math.abs(time2 - time1)); //record time passed for the tap    
+            } 
         }
     
     else if(greyAverage < 127.5 && listenData.blackWhite[listenData.blackWhite.length-1]===1) //was white and is now black
         {
             listenData.blackWhite.push(0); //record that image is now black 
             time2 = event.timeStamp; //Record time
-            if(time1 != 0)
-                {
-                    listenData.timeTaken.push(Math.abs(time2 - time1));
-                }
+            listenData.timeTaken.push(Math.abs(time2 - time1)); //record time passed for the half-gap/full-gap
         }
 
-    if(time1 !=0 && time2 !=0)
-    {
-        listenData.timeTaken.push(Math.abs(time2 - time1));
-    }
-    
     return listenData
     
     
