34dc4bbbf1ccb6e71effb0ed41aeefa24879f09f
Gradleee <37685721+Gradleee@users.noreply.github.com>
2018-03-27 18:59:05 +1100

    Update codeprocessor.js

--- js/codeprocessor.js
+++ js/codeprocessor.js
@@ -4,35 +4,52 @@
  * 		event will hold an Event object with the pixels in
  *   	event.detail.data and the timestamp in event.timeStamp
  */
+
+var listenData = {
+    blackWhite: [0],
+    timeTaken: ["start"],
+    halfGap: 0,
+    fullGap: 0,
+};
+
 _listen = function(event)
 {
-    let  gsArray,bwArray; //gsArray is the greyscale array, bwArray is the black and white array
-    for (var i=0;i<=(event.detail.data.length)/4;i++)//finding the greyscale translation of the RGBA data
-         {
-            gsArray[i] = (event.detail.data[0+4*i]+event.detail.data[1+4*i]+event.detail.data[2+4*i])/3;
-            //variable i represents each set of four numbers,
-            //the first three being important, are averaged to find the greyscale equivalent
-            //the fourth number is discarded
-            //examples:
-            //when i=0, data numbers 1, 2 and 3 are averaged. 
-            //when i=3, data numbers 12, 13 and 14 (4*3, 4*3+1 and 4*3+2) are averaged.
-         }
-    for (var i = 0; i < gsArray.length; i++)//finding the black or white equivalent of the greyscale data
+    
+    var greyAverage = (event.detail.data[0] + event.detail.data[1] + event.detail.data[2])/3
+    let time1 = 0, time2 = 0, tempTime = 0;
+    if(greyAverage > 127.5 && listenData.blackWhite[listenData.blackwhite.length-1]===0) //was black and is now white
+        {
+            listenData.blackWhite.push(1); //record that image is now white
+            if(time2 != 0)
+            {
+                time1 = event.timeStamp; //record time
+            }
+            
+            listenData.timeTaken.push("tap");
+        }
+    
+    else if(greyAverage < 127.5 && listenData.blackWhite[listenData.blackWhite.length-1]===1) //was white and is now black
+        {
+            listenData.blackWhite.push(0); //record that image is now black 
+            time2 = event.timeStamp; //Record time
+            if(time1 != 0)
+                {
+                    listenData.timeTaken.push(Math.abs(time2 - time1));
+                }
+        }
+
+    if(time1 !=0 && time2 !=0)
     {
-      if (gsArray[i]<127.5)
-      {
-        bwArray[i] = 0; // if the greyscale is less than half, it is black, i.e 0
-      }
-      else
-      {
-        bwArray[i] = 1; // if the greyscale is more than half, it is white, i.e 1
-      }
+        listenData.timeTaken.push(Math.abs(time2 - time1));
     }
-    return bwArray
+    
+    return listenData
+    
+    
 };
 
 /**
- * Your header documentation here for clear 
+ * Your header documentation here for clear
  */
 clear = function()
 {
@@ -41,18 +58,13 @@ clear = function()
 
 /**
  * Your header documentation here for translate
- hi*/
+ */
 translate = function()
 {
+    listenData.halfGap = listenData.tapTime;
+    listenData.fullGap = 3 * listenData.tapTime;
+    
 	// Creating the tap code array
 	let array = [["e", "t", "a", "n", "d"],["o", "i", "r", "u", "c"],["s", "h", "m", "f", "p"],
 		     ["l", "y", "g", "v", "j"], ["w", "b", "x", "q", "z"]];
-	// array[0][1]
-
-    /*
-    table =
-    {
-
-    }
-    */
 };
