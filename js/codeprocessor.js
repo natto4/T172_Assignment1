978bef2e71de9d9091658d8f3833da386023b546
natto4 <natalie.zw.lin@gmail.com>
2018-03-27 20:33:01 +1100

    step 7
    half finished step 7, ask if you dont understand where its going

--- js/codeprocessor.js
+++ js/codeprocessor.js
@@ -14,7 +14,7 @@ _listen = function(event)
             //the first three being important, are averaged to find the greyscale equivalent
             //the fourth number is discarded
             //examples:
-            //when i=0, data numbers 1, 2 and 3 are averaged. 
+            //when i=0, data numbers 1, 2 and 3 are averaged.
             //when i=3, data numbers 12, 13 and 14 (4*3, 4*3+1 and 4*3+2) are averaged.
          }
     for (var i = 0; i < gsArray.length; i++)//finding the black or white equivalent of the greyscale data
@@ -32,7 +32,7 @@ _listen = function(event)
 };
 
 /**
- * Your header documentation here for clear 
+ * Your header documentation here for clear
  */
 clear = function()
 {
@@ -55,4 +55,20 @@ translate = function()
 
     }
     */
+    let i,space=[], k=[];
+    for(i==0;i<=letters.length-2,i++) //letters is the outputted array of letters
+    {
+      if(letters.charAt(i)+letters.charAt(i+1)+letters.charAt(i+2) == "wuw"
+      {
+        space.push(i);
+      }
+    }
+    for(i==0;i<=letters.length-1,i++) //letters is the outputted array of letters
+    {
+      if(msg[i]+msg[i+1] == "qc")
+      {
+        k.push(i)
+      }
+    }
+    
 };
